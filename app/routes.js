c454fb9df0aa76f522c5a8985e9961d6f7a34b32
spaceghost28 <spaceghost28@gmail.com>
2017-01-30 19:54:51 -0500

    first commit

---
+++ app/routes.js
@@ -0,0 +1,44 @@
+module.exports = function(app) {
+
+  var User = require('./models/User');
+  var jwt = require("jsonwebtoken");
+
+  app.post('/login', function(req, res) {
+    User.findOne({email: req.body.email }, function(err, user) {
+      if (err) {
+        res.status(400).json(err);
+      } else if (!user) {
+        res.status(401).json({ "message": "invalid email"});
+      } else if (!user.checkPassword(req.body.password)){
+        res.status(401).json({ "message": "invalid password"});
+      } else {
+        var token = user.generateJwt();
+        res.json({
+          "token": token
+        });
+      }
+    });
+  });
+
+  app.post('/register', function(req, res) {
+    User.findOne({email: req.body.email}, function(err, existingUser) {
+      if (err) {
+        res.status(400).json(err);
+      } else if (existingUser) {
+        res.status(401).json({ "message": "that email is already registered"});
+      } else {
+        var user = new User();
+        user.email = req.body.email;
+        user.setPassword(req.body.password);
+        console.log(user);
+        user.save(function(err) {
+          var token = user.generateJwt();
+          res.json({
+            "token": token
+          });
+        });
+      }
+    });
+  });
+
+};
